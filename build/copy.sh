594796cef6007b194ed70ec1babfe7cdff80d30e
Yaroslav Savaryn <yariksav@gmail.com>
2019-05-18 13:07:02 +0200

    v0.3.1

--- build/copy.sh
+++ build/copy.sh
@@ -5,6 +5,9 @@ PROJECTS=($(echo $pid | tr " " "\n"))
 for project in "${PROJECTS[@]}"
 do
   COPY_PATH=$project/node_modules/$PROJECT_NAME/
+  if [[ ! -e $COPY_PATH ]]; then
+    mkdir $COPY_PATH
+  fi
   cp -r !(node_modules*|build*|coverage*|test*) $COPY_PATH
   echo "copied to $COPY_PATH"
 done
